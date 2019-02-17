80622f959c9f05b315ccdb16b4a559efa0899400
SJ-Dev94 <sethjonesdevelopment@gmail.com>
2019-02-17 12:50:04 -0500

    current working

---
+++ client/src/history.js
@@ -0,0 +1,5 @@
+//Bespoke history object created for me to maintain rather than react router's history object
+
+import createHistory from 'history/createBrowserHistory';
+
+export default createHistory();
