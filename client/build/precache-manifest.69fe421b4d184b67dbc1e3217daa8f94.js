d8970b5bd65203b5ced90c13bcaa34ccd0ecd212
SJ-Dev94 <sethjonesdevelopment@gmail.com>
2019-02-18 18:35:52 -0500

    added firebase hosting, working on database

---
+++ client/build/precache-manifest.69fe421b4d184b67dbc1e3217daa8f94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09c02590a8fe00d12c77",
+    "url": "/static/js/main.09c02590.chunk.js"
+  },
+  {
+    "revision": "d3b677cca63e7da92082",
+    "url": "/static/js/1.d3b677cc.chunk.js"
+  },
+  {
+    "revision": "b3c8f31848a9f1990046893dc0903b31",
+    "url": "/index.html"
+  }
+];
